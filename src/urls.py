40c376889aac901ed66155b7f093cee3d6aeab31
anonymous203030 <aram.simonyan.03@gmail.com>
2020-11-02 00:09:47 +0400

    paypal remove

--- src/urls.py
+++ src/urls.py
@@ -34,7 +34,6 @@ urlpatterns = [
 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    # path('paypal/', include('paypal.standard.ipn.urls') )
     #   DJANGO-DEBUG-TOOLBAR
      ]
 if settings.DEBUG:
